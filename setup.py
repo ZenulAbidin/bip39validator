ac1385cd8b637a8fc0b2870dc4c8ae335701ccfb
Ali Sherief <alihsherief@linuxmail.org>
2020-10-07 20:46:40 +0000

    Fix setup.py - Add "requests" to /requirements.txt - Autogenerate setup.py from PyCharm
    Signed-off-by: Ali Sherief <alihsherief@linuxmail.org>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bip39validator',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/ZenulAbidin/bip39validator',
+    license='MIT',
+    author='Ali Sherief',
+    author_email='alihsherief@linuxmail.org',
+    description='Validator for BIP39 wordlists'
+)
