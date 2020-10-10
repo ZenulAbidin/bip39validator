bbf33ac10a73c883f7c6b6b3eb41baccc387feda
Ali Sherief <alihsherief@linuxmail.org>
2020-10-10 08:18:13 +0000

    Use p-green PSphinxTheme theme
    Signed-off-by: Ali Sherief <alihsherief@linuxmail.org>

--- docs/conf.py
+++ docs/conf.py
@@ -50,9 +50,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.desktop']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'p-green'
+import os
+from PSphinxTheme import utils
+
+p, html_theme, needs_sphinx = utils.set_psphinxtheme(html_theme)
+html_theme_path = p
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
